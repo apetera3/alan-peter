9e800a01d89b38f29436786c2f02f941f3ff5caa
Alan Peter <petera3@gmatc.matc.edu>
2023-10-09 10:03:36 +0300

    completd activity 4

--- config/db.js
+++ config/db.js
@@ -1,13 +1,14 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const connectDatabase = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/your_database_name", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    });
+    await mongoose.connect(
+      "mongodb+srv://alan_peter7:qyMru3IjbVJMi89o@cluster0.yzjqt5l.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("MongoDB connected");
   } catch (error) {
     console.error(error.message);
