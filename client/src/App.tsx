88a9229e3a08f406ce2f9e09b432092c104ecdfb
Alan Peter <petera3@gmatc.matc.edu>
2023-10-25 18:55:18 +0300

    Completed Activity 4

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,30 @@
-import React from "react";
-import "./App.css";
-import axios from "axios";
+import React from 'react';
+import './App.css';
+import axios from 'axios';
 
 class App extends React.Component {
   state = {
-    data: null,
-  };
+    data: null
+  }
 
   componentDidMount() {
-    axios
-      .get("http://localhost:5000")
+    axios.get('http://localhost:5000')
       .then((response) => {
         this.setState({
-          data: response.data,
-        });
+          data: response.data
+        })
       })
       .catch((error) => {
-        console.log(`Error fetching data: ${error}`);
-      });
+        console.error(`Error fetching data: ${error}`);
+      })
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">GoodThings</header>
+        <header className="App-header">
+          GoodThings
+        </header>
         {this.state.data}
       </div>
     );
