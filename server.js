5501b9497cfa0a63018feec929a27bd450a45530
Alan Peter <petera3@gmatc.matc.edu>
2023-11-01 19:47:29 +0300

    Completed Activity 9

--- server.js
+++ server.js
@@ -6,15 +6,13 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import config from "config";
 import User from "./models/User";
+import Post from "./models/Post";
 import auth from "./middleware/auth";
 
-// Initialize express application
 const app = express();
 
-// Connect database
 connectDatabase();
 
-// Configure Middleware
 app.use(express.json({ extended: false }));
 app.use(
   cors({
@@ -22,7 +20,6 @@ app.use(
   })
 );
 
-// API endpoints
 /**
  * @route GET /
  * @desc Test endpoint
@@ -54,7 +51,6 @@ app.post(
     } else {
       const { name, email, password } = req.body;
       try {
-        // Check if user exists
         let user = await User.findOne({ email: email });
         if (user) {
           return res
@@ -62,21 +58,17 @@ app.post(
             .json({ errors: [{ msg: "User already exists" }] });
         }
 
-        // Create a new user
         user = new User({
           name: name,
           email: email,
           password: password,
         });
 
-        // Encrypt the password
         const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(password, salt);
 
-        // Save to the db and return
         await user.save();
 
-        // Generate and return a JWT token
         returnToken(user, res);
       } catch (error) {
         res.status(500).send("Server error");
@@ -115,7 +107,6 @@ app.post(
     } else {
       const { email, password } = req.body;
       try {
-        // Check if user exists
         let user = await User.findOne({ email: email });
         if (!user) {
           return res
@@ -123,7 +114,6 @@ app.post(
             .json({ errors: [{ msg: "Invalid email or password" }] });
         }
 
-        // Check password
         const match = await bcrypt.compare(password, user.password);
         if (!match) {
           return res
@@ -131,7 +121,6 @@ app.post(
             .json({ errors: [{ msg: "Invalid email or password" }] });
         }
 
-        // Generate and return a JWT token
         returnToken(user, res);
       } catch (error) {
         res.status(500).send("Server error");
@@ -157,6 +146,137 @@ const returnToken = (user, res) => {
     }
   );
 };
+
+// Post endpoints
+/**
+ * @route POST api/posts
+ * @desc Create post
+ */
+app.post(
+  "/api/posts",
+  [
+    auth,
+    [
+      check("title", "Title text is required").not().isEmpty(),
+      check("body", "Body text is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    } else {
+      const { title, body } = req.body;
+      try {
+        const user = await User.findById(req.user.id);
+
+        const post = new Post({
+          user: user.id,
+          title: title,
+          body: body,
+        });
+
+        await post.save();
+
+        res.json(post);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Server error");
+      }
+    }
+  }
+);
+
+/**
+ * @route GET api/posts
+ * @desc Get posts
+ */
+app.get("/api/posts", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+
+    res.json(posts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error");
+  }
+});
+
+/**
+ * @route GET api/posts/:id
+ * @desc Get post
+ */
+app.get("/api/posts/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    res.json(post);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error");
+  }
+});
+
+/**
+ * @route DELETE api/posts/:id
+ * @desc Delete a post
+ */
+app.delete("/api/posts/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+
+    await post.remove();
+
+    res.json({ msg: "Post removed" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error");
+  }
+});
+
+/**
+ * @route PUT api/posts/:id
+ * @desc Update a post
+ */
+app.put("/api/posts/:id", auth, async (req, res) => {
+  try {
+    const { title, body } = req.body;
+    const post = await Post.findById(req.params.id);
+
+    // Make sure the post was found
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    // Make sure the request user created the post
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+
+    // Update the post and return
+    post.title = title || post.title;
+    post.body = body || post.body;
+
+    await post.save();
+
+    res.json(post);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error");
+  }
+});
 
 // Connection listener
 const port = 5000;
